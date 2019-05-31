ea42227582d36347c3f093277c2e000cf60d6bd8
Scott Checko <schecko@sfu.ca>
2019-05-31 06:30:13 +0100

    binding to any ip

--- server/src/main.rs
+++ server/src/main.rs
@@ -8,7 +8,7 @@ fn index(_req: &HttpRequest) -> &'static str {
 fn main() {
     println!("hello world");
     server::new(|| App::new().resource("/", |r| r.f(index)))
-        .bind("127.0.0.1:8088")
+        .bind("0.0.0.0:8080")
         .unwrap()
         .run();
     println!("hello");
