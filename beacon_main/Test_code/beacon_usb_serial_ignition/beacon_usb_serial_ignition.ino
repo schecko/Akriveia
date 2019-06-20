a1b13b7bc7eacc164754e7c8a65642948cab00a1
Scott Checko <schecko@sfu.ca>
2019-06-20 14:58:06 -0700

    improving the robustness of the usb serial test

--- beacon_main/Test_code/beacon_usb_serial_ignition/beacon_usb_serial_ignition.ino
+++ beacon_main/Test_code/beacon_usb_serial_ignition/beacon_usb_serial_ignition.ino
@@ -1,30 +1,41 @@
 bool system_on = false;
 String input = "";
 
+// responses
+const String ack = "ack";
+const String nak = "nak";
+
+// commands
+const String command_start = "start";
+const String command_end = "end";
+
+int data = 0;
+
 void setup() {
   Serial.begin(9600);
-  Serial.println("Enter 'start' to start sending data");
-  Serial.println("Enter 'stop' to stop sending data");
 }
 
 void loop() {
-  
+
   if (Serial.available()>0) {
-    
+
     input = Serial.readString();
-    
-    if (input.indexOf("start")>=0){
-      Serial.println("Start Recieved, Start sending data:");
+
+    if (input.indexOf(command_start)>=0){
+      Serial.println(ack);
       system_on = true;
     }
-    else if (input.indexOf("end")>=0){
-      Serial.println("End Recieved, stop sending data:");
+    else if (input.indexOf(command_end)>=0){
+      Serial.println(ack);
       system_on = false;
+    } else {
+	  Serial.println(nak);
     }
   }
 
   if (system_on==true){
-    Serial.println("DATA");
+    Serial.println(data);
+	data++;
   }
 
   delay(500);
