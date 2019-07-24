f8dfd2163b0c263357e0140b7c386542d966be45
Jerry Liu <liuyal@sfu.ca>
2019-07-23 18:41:53 -0700

    added circle plot

--- beacon_main/beacon_test_code/esp_plot.py
+++ beacon_main/beacon_test_code/esp_plot.py
@@ -18,12 +18,19 @@ y_2 = max
 x_3 = max
 y_3 = 0.00
 
+dL1 = 0.00
+dL2 = 0.00
+dL3 = 0.00
+
 data = []
 index = []
 
 def data_maker(a1,a2,a3):
     global data
     global index
+    global dL1
+    global dL2
+    global dL3
 
     n1 = []
     n2 = []
@@ -42,12 +49,12 @@ def data_maker(a1,a2,a3):
             n1.insert(0,float(str(msg1).split('|')[-1].replace('\\r\\n\'', '')))
             n2.insert(0,float(str(msg2).split('|')[-1].replace('\\r\\n\'', '')))
             n3.insert(0,float(str(msg3).split('|')[-1].replace('\\r\\n\'', '')))
+
+            n10 = float(str(msg1).split('|')[-1].replace('\\r\\n\'', ''))
+            n20 = float(str(msg2).split('|')[-1].replace('\\r\\n\'', ''))
+            n30 = float(str(msg3).split('|')[-1].replace('\\r\\n\'', ''))
         except:
             None
-
-        n10 = float(str(msg1).split('|')[-1].replace('\\r\\n\'', ''))
-        n20 = float(str(msg2).split('|')[-1].replace('\\r\\n\'', ''))
-        n30 = float(str(msg3).split('|')[-1].replace('\\r\\n\'', ''))
 
         n11 = sum(n1)/len(n1)
         n21 = sum(n2)/len(n2)
@@ -56,7 +63,11 @@ def data_maker(a1,a2,a3):
         d1 = 10.00 ** ((measure_power - (n11)) / (10.00 * N))
         d2 = 10.00 ** ((measure_power - (n21)) / (10.00 * N))
         d3 = 10.00 ** ((measure_power - (n31)) / (10.00 * N))
-        
+
+        dL1 = d1
+        dL2 = d2
+        dL3 = d3
+
         print(str(n1) + "\n" + str(n2) + "\n" + str(n3))
         print("AVG: " + str(n11) + ' ' + str(n21) + ' ' + str(n31))
         print("D: " + str(d1) + " | " + str(d2) + " | " + str(d3))
@@ -73,14 +84,10 @@ def data_maker(a1,a2,a3):
 
         print(str(x) + '|' + str(y))
         try:
-            if len(n1) >= size:
-                index = []
-                index.append(abs(x))
-            if len(n1) >= size:
-                data = []
-                data.append(abs(y))
-        except:
-            None
+            if len(n1) >= size: index = []; index.append(abs(x))
+            if len(n1) >= size: data = []; data.append(abs(y))
+        except: None
+
         time.sleep(1)
 
 
@@ -94,14 +101,22 @@ def plotter():
             elif x[0] > max: x = max
             if y[0] < min: y = min
             elif y[0] > max: y = max
-        except:
-            None
-
+        except: None
 
         ax1.clear()
-        ax1.set_ylim(bottom=0, top=10)
-        ax1.set_xlim(left=0, right=10)
+        ax1.set_ylim(bottom=0, top=5)
+        ax1.set_xlim(left=0, right=5)
         ax1.scatter(x, y)
+
+        circle1 = plt.Circle((0, 0), dL1, fc='r', ec='r', lw=2, alpha=0.5)
+        circle2 = plt.Circle((0, 3), dL2, fc='b', ec='b', lw=2, alpha=0.5)
+        circle3 = plt.Circle((3, 0), dL3, fc='g', ec='g', lw=2, alpha=0.5)
+
+        ax1.add_artist(circle1)
+        ax1.add_artist(circle2)
+        ax1.add_artist(circle3)
+
+
 
     style.use('seaborn-whitegrid')
     fig = plt.figure(num=1, figsize=(10,4), dpi=100)
@@ -112,8 +127,8 @@ def plotter():
 
 if __name__ == "__main__":
 
-    a1 = serial.Serial('COM11', 115200)
-    a2 = serial.Serial('COM3', 115200)
+    a1 = serial.Serial('COM4', 115200)
+    a2 = serial.Serial('COM9', 115200)
     a3 = serial.Serial('COM10', 115200)
     msg1 = b""
     msg2 = b""
