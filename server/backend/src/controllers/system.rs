c5c999fbbd86564bf5686867355c1ef74f18acb2
Scott Checko <schecko@sfu.ca>
2019-08-20 03:28:18 -0700

    working on setting up db creation/reset

--- server/backend/src/controllers/system.rs
+++ server/backend/src/controllers/system.rs
@@ -1,80 +1,86 @@
-use tokio_postgres::{NoTls};
-use futures::{ Future, };
+use tokio_postgres::{ NoTls, error::SqlState, };
+use futures::{ Future, future::Either };
 
 pub fn create_db() {
-    /*let fut = tokio_postgres::connect("dname=ak port=5432 host=localhost password=postgres user=postgres", NoTls)
-        .and_then(|_| {
-            // successfully connected with ak, we need to drop this connection, connect with the
-            // default db and then drop ak
-            */
-            let fut = tokio_postgres::connect("port=5432 host=localhost password=postgres user=postgres", NoTls)
+    println!("creating db");
+    let fut = tokio_postgres::connect("dbname=ak host=localhost password=postgres user=postgres", NoTls)
+        .then(|res| {
+            // make a connection to the default database to create the ak db
+            let connect = tokio_postgres::connect("host=localhost password=postgres user=postgres", NoTls)
                 .map(|(client, connection)| {
                     let connection = connection.map_err(|e| eprintln!("db connection error: {}", e));
                     tokio::spawn(connection);
                     client
-                })
-                .and_then(|mut client| {
-                    client.prepare("DROP DATABASE ak")
-                        .map(|statement| (client, statement))
-                })
-                .and_then(|(mut client, statement)| {
-                    client.execute(&statement, &[])
-                        .map(|row_count| {
-                            println!("successfully dropped old ak db");
-                            assert_eq!(row_count, 0);
-                            client
-                        })
-                })
-                .and_then(|mut client| {
-                    client.prepare("CREATE DATABASE ak")
-                        .map(|statement| (client, statement))
-                })
-                .and_then(|(mut client, statement)| {
-                    client.execute(&statement, &[])
-                        .map(|row_count| {
-                            println!("successfully dropped old ak db");
-                            assert_eq!(row_count, 0);
-                            client
-                        })
-                })
-                .map(|_client| {
-                })
-                .map_err(|e| {
-                    eprintln!("db error: {}", e);
                 });
-                //.map_err(|e| {
-                //    eprintln!("db error: {}", e);
-                //});
-                //.map(|_| ok(()))
-                /*
-        })
-        .map_err(|e| {
-            // its fine if the db doesnt exist, other errors are bad
-            match e.code() {
-                code => println!("code is {:?}", code),
+
+            // check if the ak connection was successful, and delete ak if necessary
+            match &res {
+                Err(e) if e.code() == Some(&SqlState::INVALID_CATALOG_NAME) => {
+                    Either::B(connect)
+                },
+                Ok(_) | Err(_) => {
+                    // successfully connected with ak, we need to drop it before continuing
+                    let prepared_drop = connect
+                        .and_then(|mut client| {
+                            client.prepare("DROP DATABASE ak")
+                                .map(|statement| (client, statement))
+                        })
+                        .and_then(|(mut client, statement)| {
+                            client.execute(&statement, &[])
+                                .map(|row_count| {
+                                    assert_eq!(row_count, 0);
+                                    client
+                                })
+                        });
+                    Either::A(prepared_drop)
+                },
             }
-            eprintln!("db error: {}", e);
         })
-        //.map(|_| ok(()))
+        // at this point, the ak database should not exist. now we can create it fresh
+        .and_then(|mut client| {
+            client.prepare("CREATE DATABASE ak")
+                .map(|statement| (client, statement))
+        })
+        .and_then(|(mut client, statement)| {
+            client.execute(&statement, &[])
+                .map(|row_count| {
+                    assert_eq!(row_count, 0);
+                    client
+                })
+        })
+        .map(|_client| {
+            // map to no return type
+        })
         .and_then(|_| {
-            tokio_postgres::connect("dbname=ak port=5432 host=localhost password=postgres user=postgres", NoTls)
+            tokio_postgres::connect("dbname=ak host=localhost password=postgres user=postgres", NoTls)
         })
         .map(|(client, connection)| {
             let connection = connection.map_err(|e| eprintln!("db connection error: {}", e));
             actix::spawn(connection);
             client
         })
-        /*.and_then(|mut client| {
-            client.execute("CREATE DATABASE ak", &[])
+        .and_then(|mut client| {
+            client
+                .prepare("
+                    CREATE TABLE beacons (
+                        mac_address macaddr PRIMARY KEY
+                    )
+                ")
+                .map(|statement| (client, statement))
         })
-        .map(|rows| {
-            println!("successfully created ak db");
-            assert_eq!(rows.len(), 0);
-        })*/
+        .and_then(|(mut client, statement)| {
+            client.execute(&statement, &[])
+                .map(|row_count| {
+                    assert_eq!(row_count, 0);
+                    client
+                })
+        })
+        .map(|_client| {
+            // map to no return type
+        })
         .map_err(|e| {
             eprintln!("db error: {}", e);
-        });*/
+        });
 
-        // By default, tokio_postgres uses the tokio crate as its runtime.
         actix::spawn(fut);
 }
