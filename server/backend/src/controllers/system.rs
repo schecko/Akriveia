db5efde8049bcfd85f8ae0b9898ab7399a27da44
Scott Checko <schecko@sfu.ca>
2019-08-21 00:08:05 -0700

    added to the schema, also added schema undo

--- server/backend/src/controllers/system.rs
+++ server/backend/src/controllers/system.rs
@@ -1,5 +1,5 @@
 use tokio_postgres::{ NoTls, error::SqlState, };
-use futures::{ Future, future::Loop, future::ok, future::Either, future::loop_fn, };
+use futures::{ Future, future::err, future::Loop, future::ok, future::Either, future::loop_fn, };
 
 fn connect_db(params: &str) -> impl Future<Item=tokio_postgres::Client, Error=tokio_postgres::Error> {
     tokio_postgres::connect(params, NoTls)
@@ -9,6 +9,46 @@ fn connect_db(params: &str) -> impl Future<Item=tokio_postgres::Client, Error=to
             client
         })
 }
+
+// dont bother undoing table creations, the entire ak database is dropped and recreated.
+const UNDO_SCHEMA: [&str; 4] = [
+    "DROP USER responder",
+    "DROP USER admin",
+    "DROP ROLE ak_responder_role",
+    "DROP ROLE ak_admin_role",
+];
+
+const SCHEMA: [&str; 7] = [
+    "CREATE TABLE maps (
+        floor_id VARCHAR(255) PRIMARY KEY,
+        blueprint BYTEA,
+        name TEXT
+    );",
+    "CREATE TABLE users (
+        id INTEGER PRIMARY KEY,
+        coordinates real[2],
+        emergency_contact INTEGER REFERENCES users(id),
+        employee_id VARCHAR(255),
+        id_tag INTEGER,
+        last_seen timestamp,
+        mac_address MACADDR,
+        map_id VARCHAR(255) REFERENCES maps(floor_id),
+        name VARCHAR(255),
+        note TEXT,
+        phone_number VARCHAR(20),
+        type INTEGER
+    );",
+    "CREATE TABLE beacons (
+        mac_address MACADDR PRIMARY KEY,
+        coordinates real[2],
+        map_id VARCHAR(255) REFERENCES maps(floor_id),
+        name VARCHAR(255)
+    );",
+    "CREATE ROLE ak_admin_role WITH LOGIN",
+    "CREATE ROLE ak_responder_role WITH LOGIN",
+    "CREATE USER admin WITH PASSWORD 'admin' SYSID 1 ROLE ak_admin_role",
+    "CREATE USER responder WITH PASSWORD NULL SYSID 2 ROLE ak_responder_role",
+];
 
 fn ensure_ak() -> impl Future<Item=(), Error=tokio_postgres::Error> {
     tokio_postgres::connect("dbname=ak host=localhost password=postgres user=postgres", NoTls)
@@ -56,32 +96,58 @@ fn ensure_ak() -> impl Future<Item=(), Error=tokio_postgres::Error> {
         })
 }
 
-const SCHEMA: [&str; 3] = [
-    "CREATE TABLE maps (
-        floor_id VARCHAR(255) PRIMARY KEY,
-        blueprint BYTEA,
-        name TEXT
-    );",
-    "CREATE TABLE users (
-        id INTEGER PRIMARY KEY,
-        coordinates real[2],
-        emergency_contact INTEGER REFERENCES users(id),
-        employee_id VARCHAR(255),
-        id_tag INTEGER,
-        last_seen timestamp,
-        mac_address MACADDR,
-        map_id VARCHAR(255) REFERENCES maps(floor_id),
-        name VARCHAR(255),
-        note TEXT,
-        phone_number VARCHAR(20)
-    );",
-    " CREATE TABLE beacons (
-        mac_address MACADDR PRIMARY KEY,
-        coordinates real[2],
-        map_id VARCHAR(255) REFERENCES maps(floor_id),
-        name VARCHAR(255)
-    );",
-];
+fn loop_db_commands(client: tokio_postgres::Client, commands: Vec<&str>, chug_along: bool) -> impl Future<Item=tokio_postgres::Client, Error=tokio_postgres::Error> + '_ {
+    loop_fn((client, commands.into_iter()), move |(mut client, mut schema_it)| {
+        let chug = chug_along;
+        let it = Box::new(schema_it.next());
+        ok::<_, tokio_postgres::Error>(it)
+            .and_then(move |it| {
+                match *it {
+                    Some(command) => {
+                        println!("executing command {}", command);
+                        Either::A(client.prepare(command)
+                            .map(|statement| (client, statement))
+                            .and_then(move |(mut client, statement)| {
+                                client.execute(&statement, &[])
+                                    .then(move |res| {
+                                        match res {
+                                            Ok(_) => {
+                                                ok(())
+                                            },
+                                            Err(e) => {
+                                                if chug {
+                                                    ok(())
+                                                } else {
+                                                    err(e)
+                                                }
+                                            },
+                                        }
+                                    })
+                                    .map(|_| {
+                                        (it, client)
+                                    })
+                            }))
+                    },
+                    None => {
+                        Either::B(ok::<_, tokio_postgres::Error>((it, client)))
+                    }
+                }
+            })
+            .and_then(|(it, client)| {
+                match *it {
+                    Some(_) => {
+                        Ok(Loop::Continue((client, schema_it)))
+                    },
+                    None => {
+                        Ok(Loop::Break((client, None)))
+                    }
+                }
+            })
+    })
+    .map(|x: (tokio_postgres::Client, Option<&str>)| {
+        x.0
+    })
+}
 
 pub fn create_db() {
     println!("creating db");
@@ -90,42 +156,12 @@ pub fn create_db() {
             connect_db("dbname=ak host=localhost password=postgres user=postgres")
         })
         .and_then(|client| {
-           loop_fn((client, SCHEMA.iter()), |(mut client, mut schema_it)| {
-                let it = Box::new(schema_it.next());
-                ok::<_, tokio_postgres::Error>(it)
-                    .and_then(|it| {
-                        match *it {
-                            Some(command) => {
-                                Either::A(client.prepare(command)
-                                    .map(|statement| (client, statement))
-                                    .and_then(|(mut client, statement)| {
-                                        client.execute(&statement, &[])
-                                            .map(|row_count| {
-                                                assert_eq!(row_count, 0);
-                                                (it, client)
-                                            })
-                                    }))
-                            },
-                            None => {
-                                Either::B(ok::<_, tokio_postgres::Error>((it, client)))
-                            }
-                        }
-                    })
-                    .and_then(|(it, client)| {
-                        match *it {
-                            Some(_) => {
-                                Ok(Loop::Continue((client, schema_it)))
-                            },
-                            None => {
-                                Ok(Loop::Break((client, None)))
-                            }
-                        }
-                    })
-            })
-            .map(|_x: (tokio_postgres::Client, Option<&str>)| {
-            })
+            loop_db_commands(client, UNDO_SCHEMA.to_vec(), true)
         })
-        .map(|_x| {
+        .and_then(|client| {
+            loop_db_commands(client, SCHEMA.to_vec(), false)
+        })
+        .map(|_| {
         })
         .map_err(|e| {
             eprintln!("db error: {}", e);
