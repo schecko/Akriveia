7d6333d4c5ab58418bd63706f5b68e57d8cb33dd
Scott Checko <schecko@sfu.ca>
2019-10-22 22:30:24 -0700

    dummy data is broken

--- server/backend/src/dummy_udp.rs
+++ server/backend/src/dummy_udp.rs
@@ -3,7 +3,8 @@ extern crate tokio;
 extern crate futures;
 extern crate bytes;
 
-use rand::Rng;
+use rand::{ Rng, SeedableRng, };
+use rand::rngs::SmallRng;
 use actix::prelude::*;
 use actix::{ Actor, Context, };
 use crate::db_utils;
@@ -12,15 +13,15 @@ use crate::models::beacon;
 use crate::beacon_manager::*;
 use common::*;
 use std::time::Duration;
-use futures::future::ok;
 
-const MESSAGE_INTERVAL: Duration = Duration::from_millis(1000);
-const MIN_DISTANCE: f64 = 0.3;
+const MESSAGE_INTERVAL: Duration = Duration::from_millis(500);
+const MIN_DISTANCE: f64 = 1.0;
 const MAX_DISTANCE: f64 = 4.0;
 
 pub struct DummyUDP {
     manager: Addr<BeaconManager>,
     data_task: SpawnHandle,
+    rng: SmallRng,
 }
 
 impl Actor for DummyUDP {
@@ -36,9 +37,11 @@ impl Handler<Internal> for DummyUDP {
     type Result = ResponseActFuture<Self, (), ()>;
 
     fn handle(&mut self, _msg: Internal, _: &mut Context<Self>) -> Self::Result {
-        let beacon_manager_addr = self.manager.clone();
-        let mut rng = rand::thread_rng();
-        let user_distance = rng.gen_range(MIN_DISTANCE, MAX_DISTANCE);
+        static mut BLEH: u32 = 0;
+        unsafe {
+            println!("bleh is : {}", BLEH);
+            BLEH += 1;
+        }
 
         let b_fut = db_utils::default_connect()
             .and_then(|client| {
@@ -50,26 +53,29 @@ impl Handler<Internal> for DummyUDP {
             });
 
         let data_gen_fut = b_fut.join(u_fut)
-            .and_then(move |((_client1, beacons), (_client2, opt_user))| {
+            .into_actor(self)
+            .and_then(move |((_client1, beacons), (_client2, opt_user)), actor, _context| {
+                let time = Utc::now();
                 if let Some(user) = opt_user {
                     for b in beacons {
-                        beacon_manager_addr
+                        let user_distance = actor.rng.gen_range(MIN_DISTANCE, MAX_DISTANCE);
+                        actor.manager
                             .do_send( TagDataMessage {
                                 data: common::TagData {
                                     beacon_mac: b.mac_address,
                                     tag_distance: user_distance,
                                     tag_mac: user.mac_address.unwrap(),
-                                    timestamp: Utc::now(),
+                                    timestamp: time,
                                 }
                             });
                     }
                 }
-                ok(())
+                fut::result(Ok(()))
             })
-            .map_err(|_err| {
+            .map_err(|_err, _actor, _context| {
             });
 
-        Box::new(data_gen_fut.into_actor(self))
+        Box::new(data_gen_fut)
     }
 }
 
@@ -102,6 +108,7 @@ impl DummyUDP {
         DummyUDP::create(move |_context| {
             DummyUDP {
                 manager,
+                rng: SmallRng::from_entropy(),
                 data_task: Default::default(),
             }
         })
