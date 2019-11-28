61483e851dd65840e52dcfeeacfe63c55ca07c7e
Jeffery <zjyeung@sfu.ca>
2019-11-27 17:41:14 -0800

    Fixed button placements

--- server/frontend/src/components/system_settings.rs
+++ server/frontend/src/components/system_settings.rs
@@ -109,43 +109,35 @@ impl Renderable<SystemSettings> for SystemSettings {
                 { if self.data.error_messages.len() > 0 { "Failure: " } else { "" } }
                 { for errors }
                 <div/>
-                <table>
-                    <tr>
-                        <td>
-                            <button
-                                class="btn btn-lg btn-primary",
-                                onclick=|_| Msg::RequestRestart(SystemCommand::StartNormal),
-                            >
-                                { "Restart Server" }
-                            </button>
-                        </td>
-                        <td>
-                            <button
-                                class="btn btn-lg btn-primary",
-                                onclick=|_| Msg::RequestRestart(SystemCommand::RebuildDB),
-                            >
-                                { "Reset Database" }
-                            </button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="formLabel">{ "Set Host IP" }</td>
-                        <td>
-                            <input
-                                type="text",
-                                class="userText",
-                                placeholder="IP address",
-                            />
-                        </td>
-                        <td>
-                            <button
-                                class="btn btn-lg btn-success",
-                            >
-                                {"Save"}
-                            </button>
-                        </td>
-                    </tr>
-                </table>
+                <h2>{ "System Settings" }</h2>
+                
+                <div class="d-flex">
+                    <button
+                        class="btn btn-lg btn-primary mr-3 my-auto",
+                        onclick=|_| Msg::RequestRestart(SystemCommand::StartNormal),
+                    >
+                        { "Restart Server" }
+                    </button>
+                    <button
+                        class="btn btn-lg btn-primary ml-3 my-auto",
+                        onclick=|_| Msg::RequestRestart(SystemCommand::RebuildDB),
+                    >
+                        { "Reset Database" }
+                    </button>
+                </div>
+
+                <div class="d-flex justify-content-start">
+                    <button
+                        class="btn btn-lg btn-secondary my-auto",
+                    >
+                        {"Set IP Address"}
+                    </button>
+                    <input
+                        type="text",
+                        class="fixedLength",
+                        placeholder="IP address",
+                    />
+                </div>
             </>
         }
     }
